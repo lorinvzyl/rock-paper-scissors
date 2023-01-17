335676ebbaea3e7e18191d36b73a154facb5cb51
lorinvzyl <lorinvzyl@protonmail.com>
2023-01-17 15:49:14 +0200

    Add basic javascript functionality

---
+++ js/script.js
@@ -0,0 +1,80 @@
+function getComputerChoice()
+{
+    var choice = Math.floor(Math.random() * 3);
+    switch (choice) {
+        case 0:
+            return "Rock";
+        case 1:
+            return "Paper";
+        case 2:
+            return "Scissors";
+        default:
+            return "Error";
+    }
+}
+
+function playRound(playerSelection, computerSelection)
+{
+
+    if(playerSelection.toLowerCase() === "rock")
+    {
+        if(computerSelection === "Paper")
+        {
+            return 0;
+        } else if(computerSelection === "Scissors")
+        {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+
+    if(playerSelection.toLowerCase() === "paper")
+    {
+        if(computerSelection === "Scissors")
+        {
+            return 0;
+        } else if(computerSelection === "Rock")
+        {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+
+    if(playerSelection.toLowerCase() === "scissors")
+    {
+        if(computerSelection === "Rock")
+        {
+            return 0;
+        } else if(computerSelection === "Paper")
+        {
+            return 1;
+        } else {
+            return 2
+        }
+    }
+}
+
+function game() {
+    let userScore = 0;
+    let computerScore = 0;
+    for(let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Choose your weapon:");
+        let computerSelection = getComputerChoice();
+        let result = playRound(playerSelection, computerSelection);
+        if(result === 0)
+            computerScore++;
+        else if(result === 1)
+            userScore++;
+        console.log(userScore + " " + playerSelection + " : " + computerScore + " " + computerSelection);
+    }
+    if(user > computer)
+        return "You win!";
+    else if (computer > user)
+        return "Computer wins!";
+    else
+        return "it's a tie!";
+}
+
+console.log(game());
